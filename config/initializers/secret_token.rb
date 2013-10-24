d96b07e0a3e07f399a50987b2689b4bd6926e213
Jordan Dash <jordan.dash92@gmail.com>
2013-10-24 14:16:33 -0500

    Initial commit of Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntroToRailsChallenge::Application.config.secret_token = '59be644afb9b0e264f08ba5c97a6aac76c155997f667cc6fca5348a078c94ab20eac6f29ac91b9104c4549c78049e7dd641a3249eab2caab2406d10429984d5d'
